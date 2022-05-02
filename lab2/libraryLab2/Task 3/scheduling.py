5bad8efd9b476f835c7c8b04087eaa2761a8d612
nellieengstrom <nellieengstrom@outlook.com>
2022-05-02 17:57:23 +0200

    Commented structure for lab 2

---
+++ lab2/libraryLab2/Task 3/scheduling.py
@@ -0,0 +1,21 @@
+#  Define maxsteps
+
+# Define the classes
+
+# Loop through the maxsteps
+
+# Create randomized inital 8 by 3 table of the classes
+
+# Define constraints
+
+# Check which classes are in current conflict and how many conflict, return list of them
+
+# Sort the list according to the class with the least conflicts
+
+# Question:
+# 1: How do we move the classes in this problem? Do we switch a class in conflict
+# with another class (also in conflict or not in conflict) or do we only switch
+# with the two empty blocks?
+
+
+# 2: Do we ever move the classes that has no conflict?
