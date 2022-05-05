73c14f3d46a5c6c1a58069090692bd73e6909fd7
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2022-05-05 16:55:17 +0200

    Task B done

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String sentence_a = "~sun v ~money v ice";
+		/*String sentence_a = "~sun v ~money v ice";
 		String sentence_b = "~money v ice v movie"; // Should not be included!!
 		String sentence_c = "~movie v money";
 		String sentence_d = "~movie v ~ice";
@@ -26,7 +26,7 @@ public class Main {
 		KB.add(C);
 		KB.add(D);
 		KB.add(E);
-		KB.add(F);
+		KB.add(F);*/
 
 		// String A = "a v b v ~c";
 		// String B = "c v b";
@@ -41,6 +41,51 @@ public class Main {
 		// ArrayList<Clauses> KB = new ArrayList<Clauses>(2);
 		// KB.add(a);
 		// KB.add(b);
+
+	/*******************TASK B***********************/
+	//(A -> -C & (B | -B)) & (-A -> (- C | C) & (-B | B)) & (B -> A & -C) & (-C -> A & (-B | B))
+		
+		//transform to CNF
+		// (-A V -C) &
+		// (B V -B) &
+		// (A V -C V C) &
+		// (-B V B) &
+		// (-B V A) &
+		// (B V -A) &
+		// -C &
+		// (C V A) &
+		// (-B V B)
+		//result: Clause set is true if we assign values to variables as: A -C B
+		// Alex is knight
+		// Cody is knave
+		// Ben is spy
+		String sentence_a = "~A v ~C";
+		String sentence_b = "~B"; 
+		String sentence_c = "~A v ~C v C";
+		String sentence_d = "~B v B";
+		String sentence_e = "~B v A";
+		String sentence_f = "~C";
+		String sentence_g = "C v A";
+		String sentence_h = "B v ~A";
+
+		Clauses A = new Clauses(sentence_a);
+		Clauses B = new Clauses(sentence_b);
+		Clauses C = new Clauses(sentence_c);
+		Clauses D = new Clauses(sentence_d);
+		Clauses E = new Clauses(sentence_e);
+		Clauses F = new Clauses(sentence_f);
+		Clauses G = new Clauses(sentence_g);
+		Clauses H = new Clauses(sentence_h);
+		ArrayList<Clauses> KB = new ArrayList<Clauses>(8);
+
+		KB.add(A);
+		KB.add(B);
+		KB.add(C);
+		KB.add(D);
+		KB.add(E);
+		KB.add(F);
+		KB.add(G);
+		KB.add(H);
 
 		Clauses res = new Clauses();
 		ArrayList<Clauses> result = new ArrayList<Clauses>();
