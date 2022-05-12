147feb19af8785d5d3548062fd54c9af97a25b80
Anna Wästling <45712410+AnnaWastling@users.noreply.github.com>
2022-05-12 10:01:43 +0200

    something wrong

--- lab4/shakey.pl
+++ lab4/shakey.pl
@@ -4,22 +4,18 @@
 %go(x,y)
 act(go(X,Y),
 [location(X), location(Y), door(X,Y), shakey(X), on(floor)],
-[],
+[shakey(X)],
 [shakey(Y)]
 ).
-
 %push(b,x,y)
 act(push(B,X,Y),
-[box(B), boxpos(B,X), door(X,Y), shakey(X), location(X), location(Y), handEmpty, switch(S), switchPos(S,X), switched(S,on), on(floor) ],
-[boxpos(B,X),shakey(X), handEmpty],
-[shakey(Y), boxpos(B,Y)]
+[box(B), shakey(X), boxpos(B,X), door(X,Y), switch(S), switchPos(S,X), switched(S,on), on(floor) ],
+[boxpos(B,X), shakey(X)],
+[boxpos(B,Y), shakey(Y)]
 ).
-%predicate box(...)
-%lightOn=true for shakey to find a box
-%shakey must climb onto box to switch lightswitch
 %climbUp(b)
 act(climbUp(B),
-[location(X), on(floor), box(B), boxPos(B, X)],
+[shakey(X), on(floor), box(B), boxPos(B, X)],
 [on(floor)],
 [on(B)]
 ).
@@ -55,20 +51,22 @@ box(a),
 box(b),
 box(c),
 box(d),
+
 boxPos(a, room1),
 boxPos(b, room1),
 boxPos(c, room1),
 boxPos(d, room1),
-handEmpty,
 
 switch(one),
 switch(two),
 switch(three),
 switch(four),
+
 switched(one, on),
 switched(two, off),
 switched(three, off),
 switched(four, on),
+
 switchPos(one, room1),
 switchPos(two, room2),
 switchPos(three, room3),
@@ -78,14 +76,14 @@ location(room1),
 location(room2),
 location(room3),
 location(room4),
-%location(corridor),
+
 door(room1,corridor),
 door(room2,corridor),
 door(room3,corridor),
 door(room4,corridor),
+
 door(corridor, room1),
 door(corridor, room2),
 door(corridor, room3),
 door(corridor, room4)
-
 ]).
