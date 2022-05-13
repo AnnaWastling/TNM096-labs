288462532f534f37df71b2956b000167762778cb
nellieengstrom <45712511+nellieengstrom@users.noreply.github.com>
2022-05-13 11:48:51 +0200

    Cleaned version

--- lab4/shakey.pl
+++ lab4/shakey.pl
@@ -3,16 +3,10 @@
 %actions
 %go(x,y)
 act(go(X,Y),
-[connected(X,Y), in(s,X), on(floor)],    %consider remove shakey as var
+[connected(X,Y), in(s,X), on(floor)],
 [in(s,X)],
 [in(s,Y)]
 ).
-
-%act(pushInRoom(B,X,Y),
-%[box(B), boxPos(B,X), same(Y,X), in(s,X), on(floor), switch(S), switchPos(S,X), switched(S,on)],%switch(S), switchPos(S,X), switched(S,on),  sameroom(X,Y)
-%[in(s, X), boxPos(B,X)],
-%[in(s, Y), boxPos(B,Y), underSwitch]
-%).
 
 %push(b,x,y)
 act(push(B,X,Y),
@@ -23,7 +17,7 @@ act(push(B,X,Y),
 
 %climbUp(b)
 act(climbUp(B),
-[in(s,X), on(floor), box(B), in(B, X)],    %location(X), location(LP), connected(Y,X),
+[in(s,X), on(floor), box(B), in(B, X)],
 [on(floor)],
 [on(B)]
 ).
@@ -35,7 +29,7 @@ act(climbDown(B),
 ).
 %turnOn(s)
 act(turnOn(S),
-[box(B), on(B), in(B, S), location(X), connected(S, X), switch(S), switchedOff(X)], %location(X), switch(S), switchPos(S,X), switched(S,off),
+[box(B), on(B), in(B, S), location(X), connected(S, X), switch(S), switchedOff(X)],
 [switchedOff(X)],
 [switchedOn(X)]
 ).
@@ -48,9 +42,9 @@ act(turnOff(S),
 
 %States
 goal_state(
-[in(b2, room2)]%in(s, room1), switchedOff(room1), in(b2, room2)
-).                  %make switchpositions  constants
-                     %box must be at s of that room
+[switchedOn(room2)]%in(s, room1), switchedOff(room1), in(b2, room2)
+).
+
 initial_state([
 shakey(s),
 floor,
@@ -71,28 +65,11 @@ switch(s2),
 switch(s3),
 switch(s4),
 
-%switched(s1, on),
-%switched(s2, off),
-%switched(s3, off),
-%switched(s4, on),
-
 switchedOn(room1),
 switchedOff(room2),
 switchedOff(room3),
 switchedOn(room4),
 switchedOn(corridor),
-
-%switchPos(s1, room1),
-%switchPos(s2, room2),
-%switchPos(s3, room3),
-%switchPos(s4, room4),
-
-%location(underS1),
-%location(underS2),
-%location(underS3),
-%location(underS4),
-
-underSwitch,
 
 location(room1),
 location(room2),
@@ -126,8 +103,4 @@ connected(corridor, s2),
 connected(corridor, s3),
 connected(corridor, s4)
 
-%door(underS1,corridor),
-%door(underS2,corridor),
-%door(underS3,corridor),
-%door(underS4,corridor)
 ]).
